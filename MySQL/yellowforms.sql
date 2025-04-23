f5d6b0aa7e5b540f979a5d7274a93f5b942e265c
deyyybid <deybid.eyron21@gmail.com>
2025-04-23 23:15:30 +0800

    Update yellowform.sql

--- MySQL/yellowforms.sql
+++ MySQL/yellowforms.sql
@@ -29,26 +29,26 @@ VALUES
 
 SELECT * FROM student_info;
 
-CREATE TABLE student_files (
+CREATE TABLE student_subject (
     form_id INT PRIMARY KEY,
     student_id INT,
-    request_type VARCHAR(99),
-    request_date DATE,
-    request_status VARCHAR(10),
+    subject_code VARCHAR(99),
+    teacher VARCHAR(99),
+    subject_output VARCHAR(10),
     FOREIGN KEY (student_id) REFERENCES student_info(student_id)
 );
 
-INSERT INTO student_files (form_id, student_id, request_type, request_date, request_status)
+INSERT INTO student_subject (form_id, student_id, subject_code, teacher, subject_output)
 VALUES
-    (1, 1, "Good Moral", "2025-04-01", "Pending"),
-    (2, 2, "Transcript", "2025-04-02", "Approved"),
-    (3, 3, "ID Replacement", "2025-04-03", "Pending"),
-    (4, 4, "Good Moral", "2025-04-04", "Rejected"),
-    (5, 5, "Transcript", "2025-04-05", "Approved"),
-    (6, 6, "ID Replacement", "2025-04-06", "Pending"),
-    (7, 7, "Good Moral", "2025-04-06", "Approved"),
-    (8, 8, "Transcript", "2025-04-07", "Pending"),
-    (9, 9, "Good Moral", "2025-04-08", "Approved"),
-    (10, 10, "ID Replacement", "2025-04-09", "Rejected");
+    (1, 1, "IT 424", "Engr. Mariquina", "Passed"),
+    (2, 2, "ET 421", "Engr. Mariquina", "Failed"),
+    (3, 3, "FIL 111", "Ms. Villegas", "Failed"),
+    (4, 4, "LIT 221", "Ms. Macalintal", "Failed"),
+    (5, 5, "ELEC 321", "Mr. Arienza", "Passed"),
+    (6, 6, "CANVA", "Mr. Floyd Acedo", "Pending"),
+    (7, 7, "IT 228", "Mr. Arienza", "Passed"),
+    (8, 8, "NT 424", "Mr. Ronario", "Pending"),
+    (9, 9, "GE 226", "Ms Villegas", "Passed"),
+    (10, 10, "PRO 423", "Mr. Difontorum", "Pending");
 
-SELECT * FROM student_files;
+SELECT * FROM student_subject;
